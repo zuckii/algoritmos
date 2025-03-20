81bfe96f8073f5d17d5d266f669fb52d71313557
Victor Zuchi <zuchii.victor@gmail.com>
2025-03-20 15:54:07 -0300

    Split em python e C

---
+++ split.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <locale.h>
+
+int main() {
+    char frase[500];
+
+    strcpy(frase, "A legítima lingüiça, cuidadosamente frita, exibia um gosto único e freqüente nas reuniões de ürsula e suas amigas.");
+    int len = strlen(frase);
+    printf("Ta aqui a frase: %s\n", frase);
+
+    for (int i = 0; (i < len-1); i++){
+        // printf("%c\n", frase[i]);
+        if (frase[i] == "ü"){
+            frase[i] = "u";
+        }
+    }
+
+
+    printf("Ta aqui a frase: %s", frase);
+    return 0;
+}
---
+++ split.py
@@ -0,0 +1,7 @@
+frase = "Olá tudo bem com você"
+for i in frase:
+    print(i)
+
+frase = frase.replace("á", "a")
+frase = frase.replace("ê", "e")
+print(frase)
